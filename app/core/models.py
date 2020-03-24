2b0d013261f8e84c9a292d3966267454e11bf24d
sheshagiri <seshgiriseshgiri@gmail.com>
2020-03-24 11:32:51 +0530

    removed flake8 issues and added migration file

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser, 
-    BaseUserManager, 
+    AbstractBaseUser,
+    BaseUserManager,
     PermissionsMixin)
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
