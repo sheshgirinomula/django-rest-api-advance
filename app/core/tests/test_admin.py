28cbdcf7ee10c01d5542b264c6e22854fc3faa47
sheshagiri <seshgiriseshgiri@gmail.com>
2020-03-26 11:41:05 +0530

    added featurs and fixed test cases faliure

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,8 +1,8 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
 # write your test case here
+
 
 class AdminSiteTests(TestCase):
     """"""
